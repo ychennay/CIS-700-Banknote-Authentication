715cd1c33787f7456a133ee66cd45d6a9c5d4c13
Yu Chen <yu.chen@operam.com>
2018-06-05 22:51:18 -0700

    added research paper frog boiling attack considerations

--- classifier.py
+++ classifier.py
@@ -19,15 +19,15 @@ if __name__ == "__main__":
         for idx in range(0, STEPS):
 
             step = -distance / STEPS
-            print("before", closest_positive)
+            print("before closest positive point:", closest_positive)
             closest_positive += step
-            print("after", closest_positive)
+            print("after, closest positive point:", closest_positive)
             print("negative mean:", negative_mean)
             print("distance from negative centroid: ", numpy.linalg.norm(closest_positive - negative_mean))
 
             preds = predictor.classifier.predict(closest_positive.reshape(1,-1))
             print(f"Prediction", preds)
-            if preds[0] == 1:
+            if preds[0] == 1: # only if we receive a positive classification
                 print(predictor.training_features.shape)
                 new_feature_space = np.vstack((predictor.training_features, closest_positive))
                 new_class = np.append(predictor.training_class, 1)
@@ -37,11 +37,9 @@ if __name__ == "__main__":
                 predictor.training_class = new_class
                 predictor.classifier.fit(new_feature_space, new_class)
                 input()
-            else:
+            else: # attacker is caught!
                 print("Classified as negative :(")
                 break
-
-
 
         print("\n\nClassifier: {}\n------------------------------".format(cname))
         print_data('Training', result['training'])
