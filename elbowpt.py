44bea6606ec67558779ba7df83f674eeb081eb37
Alice Chang <achang@syr.edu>
2018-06-07 17:31:14 -0400

    Made some changes to the code to be less buggy.

--- elbowpt.py
+++ elbowpt.py
@@ -1,6 +1,7 @@
 from BanknoteClassifier import *
 import json
 import numpy as np
+import matplotlib.pyplot as plot
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, mutual_info_score, recall_score
 
@@ -17,23 +18,37 @@ if __name__ == "__main__":
     # for each classifier of our choosing
     for cname in ['knn', 'linear', 'random_forest', 'neural_network', 'gaussian']:
         # interate 100 times, classifying, and corrupting the dataset using previous classification results
-
+        split = []
+        train_accuracy = []
+        test_accuracy = []
         print("Classifier: {}".format(cname.upper()))
         i=0
         for trainset in range(20, 100, 3):
             i += 1
             cl.adjust_train_test_split(trainset*0.01)
             result = cl.Classify(classifier=cname)
-            print("--- Iteration {} ---".format(i))
-            print_data('Training', result['training'])
-            print_data('Test', result['test'])
-
-            plotthis.append( {
+            # ---- iterative debugging ----
+            # print("--- Iteration {} ---".format(i))
+            # print_data('Training', result['training'])
+            # print_data('Test', result['test'])
+            # ---- json printout ----
+            run = {
                 'classifier': cname,
                 'train/test split': '{}%'.format(trainset),
                 'train set': result['training']['total_count'],
                 'train accuracy' : result['training']['accuracy']*100,
                 'test set' : result['test']['total_count'],
-                'test accuracy' : result['test']['accuracy']*100,
-            })
-        print(json.dumps(plotthis, indent=3))
+                'test accuracy' : result['test']['accuracy']*100
+            }
+
+            # ---- X/Y plotting ----
+            split.append(trainset)
+            train_accuracy.append(run['train accuracy'])
+            test_accuracy.append(run['test accuracy'])
+            print(json.dumps(run, indent=3))
+
+        plot.plot(split, train_accuracy, 'ro', split, test_accuracy, 'g^')
+        plot.xlabel('Split ratio')
+        plot.ylabel('Accuracy')
+        plot.show()
+        input()
